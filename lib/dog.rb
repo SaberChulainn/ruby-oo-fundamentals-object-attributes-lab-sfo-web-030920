a21bb9366f52e5604822a16ea0ab661d61d317fc
Ty Bandachit <tbandachit@gmail.com>
2020-03-10 22:43:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,14 @@
 class Dog 
-  
-  attr_accessor :name, :breed
   def initialize(name, breed)
     @name = name
+    @breed = breed
+  end
+  
+  def breed(breed)
+    @breed
+  end
+  
+  def breed=(breed)
     @breed = breed
   end
   
